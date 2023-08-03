fb84e4acf2491257dd84a5f14d130e4430d09202
Leandro Vandari <90213288+LeandroVandari@users.noreply.github.com>
2023-08-03 08:31:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def main():
   numero_pergunta = 1
 
   print("""Bem vindo a Quem Quer Ser Um Cerratense, estas são as regras do jogo:
-  \t1. O jogo consiste de 15 perguntas sobre o cerrado brasileiro, em ordem ascendente de dificuldade.
-  \t2. A cada pergunta, aumenta o seu prêmio.
-  \t3. A cada 5 perguntas, há um checkpoint. Se você errar, ganhará o prêmio do checkpoint anterior.
-  \t4. A cada pergunta, você pode desistir ou continuar, antes de responder. Se desistir, ficará com o prêmio correspondente à pergunta em que está.""")
+  1. O jogo consiste de 15 perguntas sobre o cerrado brasileiro, em ordem ascendente de dificuldade.
+  2. A cada pergunta, aumenta o seu prêmio.
+  3. A cada 5 perguntas, há um checkpoint. Se você errar, ganhará o prêmio do checkpoint anterior.
+  4. A cada pergunta, você pode desistir ou continuar, antes de responder. Se desistir, ficará com o prêmio correspondente à pergunta em que está.""")
 
   while numero_pergunta <= 15:
     if numero_pergunta <= 5:
@@ -24,7 +24,9 @@ def main():
       pergunta = random.choice(list(perguntas_medias.items()))
     elif numero_pergunta <= 15:
       pergunta = random.choice(list(perguntas_dificeis.items()))
-    print(f"""Pergunta número {numero_pergunta}: 
-    {pergunta}""")
+    questao = pergunta[0]
+    print(f"""\nPergunta número {numero_pergunta}: 
+    {questao}\nAs alternativas são:\n""")
+    
 if __name__ == "__main__":
   main()
